6c86ba3fbe5ea0b73860904016cc4c9cf3b39f11
Gabriel Nardes <gabrielnardeslobao@gmail.com>
2021-05-19 20:25:15 -0300

    Add print() and toString() tests

--- src/test/java/data/DataTest.java
+++ src/test/java/data/DataTest.java
@@ -3,6 +3,9 @@ package data;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DataTest {
@@ -23,5 +26,46 @@ class DataTest {
         Data data = new Data(24, 26, 22);
 
         assertEquals(data.length(), 3, "Length must be equal");
+    }
+
+    @Test
+    @DisplayName("Testing print() method")
+    public void testPrint() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        Data data = new Data(24, 26, 22, 25, 19, 31, 26, 18, 29, 24, 30, 23);
+        data.print();
+
+        String expectedOutput  = "\nDATA\n" +
+                "d[1]: 24,0\n" +
+                "d[2]: 26,0\n" +
+                "d[3]: 22,0\n" +
+                "d[4]: 25,0\n" +
+                "d[5]: 19,0\n" +
+                "d[6]: 31,0\n" +
+                "d[7]: 26,0\n" +
+                "d[8]: 18,0\n" +
+                "d[9]: 29,0\n" +
+                "d[10]: 24,0\n" +
+                "d[11]: 30,0\n" +
+                "d[12]: 23,0\n";
+
+        assertEquals(expectedOutput, outContent
+                        .toString()
+                        .replaceAll("\r", ""),
+                "Console output must be equal");
+    }
+
+    @Test
+    @DisplayName("Testing toString() method")
+    public void testToString() {
+        Data data = new Data(24, 26, 22, 25, 19, 31, 26, 18, 29, 24, 30, 23);
+
+        String expectedOutput  = "Data{data=[24.0, 26.0, 22.0, 25.0, 19.0, 31.0, 26.0, 18.0, 29.0, 24.0, 30.0, 23.0]}";
+
+        assertEquals(expectedOutput, data.toString()
+                        .replaceAll("\r", ""),
+                "Strings must be equal");
     }
 }
