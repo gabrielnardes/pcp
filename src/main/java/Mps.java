7981dea0da49519ad1e53fa868ced0be46b3efa4
Gabriel Nardes <gabrielnardeslobao@gmail.com>
2021-05-18 23:33:03 -0300

    Add Master Production Schedule

---
+++ src/main/java/Mps.java
@@ -0,0 +1,73 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Mps {
+    private final Map<String, MpsProduct> mps = new HashMap<>();
+
+    public Mps(Item item, double ... grossRequirement) {
+        createMap(item);
+        calculateMpsProduct(item, grossRequirement);
+    }
+
+    public void calculateMpsProduct(Item item, double[] grossRequirement) {
+        MpsProduct mpsProduct;
+
+        if (mps.get(item.getName()) == null) {
+            mpsProduct = new MpsProduct(item, grossRequirement);
+            mps.put(item.getName(), mpsProduct);
+        } else {
+            MpsProduct OldMps = mps.get(item.getName());
+            mpsProduct = new MpsProduct(item, grossRequirement);
+            mps.put(item.getName(), sum(OldMps, mpsProduct));
+        }
+
+        for (Child c : item.getChildren()) {
+            calculateMpsProduct(c.getItem(), scaleArray(mpsProduct.getPlannedOrderRelease(), c.getQuantity()));
+        }
+    }
+
+    public MpsProduct sum(MpsProduct OldMps, MpsProduct newMps) {
+        MpsProduct sumMps = new MpsProduct(OldMps.getItem(), sumTwoArrays(OldMps.getGrossRequirement(), newMps.getGrossRequirement()));
+
+        sumMps.setScheduledReceipts(sumTwoArrays(OldMps.getScheduledReceipts(), newMps.getScheduledReceipts()));
+        sumMps.setProjectedAvailable(sumTwoArrays(OldMps.getProjectedAvailable(), newMps.getProjectedAvailable()));
+        sumMps.setPlannedOrderRelease(sumTwoArrays(OldMps.getPlannedOrderRelease(), newMps.getPlannedOrderRelease()));
+
+        return sumMps;
+    }
+
+    public double[] scaleArray(double[] array, int scaleFactor) {
+        double[] scaledArray = new double[array.length];
+        for (int i = 0; i < array.length; i++) {
+            scaledArray[i] = array[i] * scaleFactor;
+        }
+
+        return scaledArray;
+    }
+
+    public double[] sumTwoArrays(double[] arrayA, double[] arrayB) {
+        double[] sumArray = new double[arrayA.length];
+
+        for (int i = 0; i < sumArray.length; i++) {
+            sumArray[i] = arrayA[i] + arrayB[i];
+        }
+
+        return sumArray;
+    }
+
+    public void print() {
+        System.out.print("Printing map\n");
+
+        for( Map.Entry<String, MpsProduct> entry : mps.entrySet() ){
+            entry.getValue().print();
+        }
+    }
+
+    public void createMap(Item item) {
+        mps.put(item.getName(), null);
+
+        for (Child c : item.getChildren()) {
+            createMap(c.getItem());
+        }
+    }
+}
---
+++ src/main/java/MpsProduct.java
@@ -0,0 +1,104 @@
+import java.util.Arrays;
+
+public class MpsProduct {
+    private final double[] grossRequirement;
+    private double[] scheduledReceipts;
+    private double[] projectedAvailable;
+    private double[] plannedOrderRelease;
+    private final Item item;
+
+    public MpsProduct(Item item, double[] grossRequirement) {
+        this.item = item;
+        this.grossRequirement = grossRequirement;
+        int period = grossRequirement.length;
+        this.projectedAvailable = new double[period];
+        this.scheduledReceipts = new double[period];
+        this.plannedOrderRelease = new double[period];
+
+        projectedAvailable[0] = item.getInitialStock();
+
+        for (int i = 1; i < period; i++) {
+            if (grossRequirement[i] == 0) {
+                projectedAvailable[i] = projectedAvailable[i-1];
+            } else if (projectedAvailable[i - 1] - grossRequirement[i] < item.getSafetyStock()){
+                double lot = Math.ceil((item.getSafetyStock() + grossRequirement[i] - projectedAvailable[i - 1]) / item.getLotSize());
+
+                scheduledReceipts[i] = lot * item.getLotSize();
+                plannedOrderRelease[i - (int)item.getLeadTime()] = scheduledReceipts[i];
+                projectedAvailable[i] = projectedAvailable[i - 1] + scheduledReceipts[i] - grossRequirement[i];
+            } else {
+                projectedAvailable[i] = projectedAvailable[i - 1] - grossRequirement[i];
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "MpsProduct{" +
+                "grossRequirement=" + Arrays.toString(grossRequirement) +
+                "\nscheduledReceipts=" + Arrays.toString(scheduledReceipts) +
+                "\nprojectedAvailable=" + Arrays.toString(projectedAvailable) +
+                "\nplannedOrderRelease=" + Arrays.toString(plannedOrderRelease) +
+                '}';
+    }
+
+    public void print() {
+        System.out.printf("Item: %s\n", item.getName());
+
+        System.out.print("Gross Requirement     ");
+        for (double el : grossRequirement) {
+            System.out.printf("%5.0f ", el);
+        }
+        System.out.print("\n");
+
+        System.out.print("Scheduled Receipts    ");
+        for (double el : scheduledReceipts) {
+            System.out.printf("%5.0f ", el);
+        }
+        System.out.print("\n");
+
+        System.out.print("Projected Available   ");
+        for (double el : projectedAvailable) {
+            System.out.printf("%5.0f ", el);
+        }
+        System.out.print("\n");
+
+        System.out.print("Planned Order Release ");
+        for (double el : plannedOrderRelease) {
+            System.out.printf("%5.0f ", el);
+        }
+        System.out.print("\n\n");
+    }
+
+    public double[] getPlannedOrderRelease() {
+        return plannedOrderRelease;
+    }
+
+    public double[] getGrossRequirement() {
+        return grossRequirement;
+    }
+
+    public double[] getScheduledReceipts() {
+        return scheduledReceipts;
+    }
+
+    public void setScheduledReceipts(double[] scheduledReceipts) {
+        this.scheduledReceipts = scheduledReceipts;
+    }
+
+    public double[] getProjectedAvailable() {
+        return projectedAvailable;
+    }
+
+    public void setProjectedAvailable(double[] projectedAvailable) {
+        this.projectedAvailable = projectedAvailable;
+    }
+
+    public void setPlannedOrderRelease(double[] plannedOrderRelease) {
+        this.plannedOrderRelease = plannedOrderRelease;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+}
