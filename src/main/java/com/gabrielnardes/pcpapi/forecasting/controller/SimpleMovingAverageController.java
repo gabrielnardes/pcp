5976860bde30698c06b60b77f7381b89b3d17aa4
Gabriel Nardes <gabrielnardeslobao@gmail.com>
2021-05-31 22:50:46 -0300

    Add post and get to SMA class

--- src/main/java/com/gabrielnardes/pcpapi/forecasting/controller/SimpleMovingAverageController.java
+++ src/main/java/com/gabrielnardes/pcpapi/forecasting/controller/SimpleMovingAverageController.java
@@ -1,20 +1,32 @@
 package com.gabrielnardes.pcpapi.forecasting.controller;
 
-import com.gabrielnardes.pcpapi.forecasting.dto.SimpleMovingAverageDTO;
-import com.gabrielnardes.pcpapi.forecasting.service.SimpleMovingAverageService;
+import com.gabrielnardes.pcpapi.forecasting.entity.SimpleMovingAverage;
+import com.gabrielnardes.pcpapi.forecasting.repository.SimpleMovingAverageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/forecasting/sma")
 public class SimpleMovingAverageController {
-
-    @PostMapping()
-    public String createSimpleMovingAverage(@RequestBody SimpleMovingAverageDTO simpleMovingAverageDTO) {
-        return simpleMovingAverageService.createSimpleMovingAverage(simpleMovingAverageDTO);
-    }
+    @Autowired
+    private SimpleMovingAverageRepository simpleMovingAverageRepository;
 
     @GetMapping()
-    public String hello() {
-        return "Hello: Simple Moving Average";
+    public @ResponseBody Iterable<SimpleMovingAverage> getAll() {
+        // This returns a JSON or XML with the users
+        return simpleMovingAverageRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public @ResponseBody
+    Optional<SimpleMovingAverage> findById(@PathVariable Long id) {
+        return simpleMovingAverageRepository.findById(id);
+    }
+
+    @PostMapping()
+    public @ResponseBody SimpleMovingAverage add(@RequestBody SimpleMovingAverage sma) {
+        return simpleMovingAverageRepository.save(sma);
     }
 }
