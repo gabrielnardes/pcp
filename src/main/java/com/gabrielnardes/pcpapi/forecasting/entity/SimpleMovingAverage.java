1a7c2a63d2f609ad1b780b552c7a4a3fa1e6dfd7
Gabriel Nardes <gabrielnardeslobao@gmail.com>
2021-06-01 11:26:26 -0300

    Add lombok @Data to SMA

--- src/main/java/com/gabrielnardes/pcpapi/forecasting/entity/SimpleMovingAverage.java
+++ src/main/java/com/gabrielnardes/pcpapi/forecasting/entity/SimpleMovingAverage.java
@@ -1,6 +1,7 @@
 package com.gabrielnardes.pcpapi.forecasting.entity;
 
 import com.vladmihalcea.hibernate.type.array.DoubleArrayType;
+import lombok.Data;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 import org.hibernate.annotations.TypeDefs;
@@ -8,6 +9,7 @@ import org.hibernate.annotations.TypeDefs;
 import javax.persistence.*;
 
 @Entity
+@Data
 @TypeDefs({
         @TypeDef(
                 name = "double-array",
@@ -20,9 +22,8 @@ public class SimpleMovingAverage {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-//    @Column(nullable = false)
     @Type( type = "double-array" )
-    @Column(
+    @Column(nullable = false,
             columnDefinition = "double precision[]"
     )
     private double[] data;
@@ -35,13 +36,6 @@ public class SimpleMovingAverage {
 
     @Column(nullable = false)
     private int period;
-
-    public SimpleMovingAverage(double[] data, int period) {
-        this.data = data;
-        this.period = period;
-
-        sma = new double[(int) this.data.length - period + 1];
-    }
 
     public void print() {
         System.out.println("SMA with period " + period);
@@ -68,10 +62,6 @@ public class SimpleMovingAverage {
         return sma[i];
     }
 
-    public double[] getSma() {
-        return sma;
-    }
-
     public SimpleMovingAverage() {
     }
 
@@ -84,37 +74,28 @@ public class SimpleMovingAverage {
         this.period = period;
     }
 
+    public SimpleMovingAverage(double[] data, int period) {
+        this.data = data;
+        this.period = period;
+        this.sma = new double[this.data.length - period + 1];
+    }
+
     public SimpleMovingAverage(long id, int period, double[] data) {
         this.id = id;
         this.period = period;
         this.data = data;
+        this.sma = new double[this.data.length - period + 1];
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public int getPeriod() {
-        return period;
-    }
-
-    public void setPeriod(int period) {
-        this.period = period;
-    }
-
-    public double[] getData() {
-        return data;
-    }
-
-    public void setData(double[] data) {
-        this.data = data;
-    }
-
-    public void setSma(double[] sma) {
-        this.sma = sma;
-    }
+//    public long getId() { return id; }
+//    public void setId(long id) { this.id = id; }
+//
+//    public int getPeriod() { return period; }
+//    public void setPeriod(int period) { this.period = period; }
+//
+//    public double[] getData() { return data; }
+//    public void setData(double[] data) { this.data = data; }
+//
+//    public double[] getSma() { return sma; }
+//    public void setSma(double[] sma) { this.sma = sma; }
 }
