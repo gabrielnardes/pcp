0d326375d00e2653586a34bfb05d219bcfd009a2
Gabriel Nardes <gabrielnardeslobao@gmail.com>
2021-06-11 03:56:56 -0300

    Add Child Entity

--- src/main/java/com/gabrielnardes/pcpapi/masterProductionSchedule/entity/Child.java
+++ src/main/java/com/gabrielnardes/pcpapi/masterProductionSchedule/entity/Child.java
@@ -1,7 +1,28 @@
 package com.gabrielnardes.pcpapi.masterProductionSchedule.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Child {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne()
+    @JoinColumn(name = "item_id")
     private Item item;
+
+    @ManyToOne()
+    @JoinColumn(name = "item_owner_id")
+    private Item owner;
+
+    @Column(nullable = false)
     private int quantity;
 
     public Child(Item item, int quantity) {
@@ -10,27 +31,7 @@ public class Child {
     }
 
     public void print() {
-        item.print();
+//        item.print();
         System.out.println("Quantity " + quantity);
-    }
-
-    public Item getItem() {
-        return item;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    @Override
-    public String toString() {
-        return "com.gabrielnardes.pcp.entity.masterProductionSchedule.Child{" +
-                "item=" + item +
-                ", quantity=" + quantity +
-                '}';
     }
 }
