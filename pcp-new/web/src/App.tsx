769eefc73a4562b2664286c432020ef86cb7f470
Gabriel Nardes Giampietro <gabrielnardeslobao@gmail.com>
2022-02-28 01:02:06 -0300

    Setting up react-router

--- pcp-new/web/src/App.tsx
+++ pcp-new/web/src/App.tsx
@@ -1,12 +1,27 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Home } from './pages/Home';
 
 function App() {
   return (
     <div>
-      <Home/>
+      <NavLink to='/products'>Produtos</NavLink> <br />
+      <NavLink to='/users'>Usuários</NavLink>
+      <main>
+        <Routes>
+          <Route path='/products' element={<Products />} />
+          <Route path='/users' element={<Users />} />
+        </Routes>
+      </main>
     </div>
   );
+}
+
+function Products() {
+  return <h2>Products</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
