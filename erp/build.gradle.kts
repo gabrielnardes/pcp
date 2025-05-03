c3194160b6554a327192cdd0e62d8d27d5fe498d
gabrielnardes <gabrielnardeslobao@gmail.com>
2025-05-03 11:35:38 -0300

    Add postgres database

--- erp/build.gradle.kts
+++ erp/build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.postgresql:postgresql:42.7.5")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
